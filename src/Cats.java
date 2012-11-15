b8b4f9df5dadeb50497ba73f606f891fd54f41fc
Binley <byang8@u.rochester.edu>
2012-11-15 18:09:21 -0500

    small updates to lab17

--- src/Cats.java
+++ src/Cats.java
@@ -1,77 +1,68 @@
-/* Francis Hinson, Binley Yang, Hiroyuki Asaga
- * Lab 17
- * Cats.java, CatsTest.java, lion.jpg, tiger.jpg, panther.jpg, cheetah.jpg, cat.jpg
- * November 11th 2012
- * TAs: Dan Scarafoni & Shuopeng Deng
- * CSC 171
- */
-
-import java.awt.FlowLayout; 
-import javax.swing.JFrame; 
-import javax.swing.JLabel; 
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.SwingConstants;
-import javax.swing.Icon; 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
-public class Cats extends JFrame{
-
-	private JLabel label1; 
-	private JLabel label2; 
-	private JLabel label3; 
+public class Cats extends JFrame {
+	
+	private JLabel label1;
+	private JLabel label2;
+	private JLabel label3;
 	private JLabel label4;
 	private JLabel label5;
-	 
-	public Cats(){
+	
+	public Cats() {
+		
 		super ("Wild Cats");
-		setLayout( new FlowLayout() );
+		setLayout(new FlowLayout());
 		
-		Icon lion = new ImageIcon( getClass().getResource("lion.jpg") );
-		Icon tiger = new ImageIcon( getClass().getResource("tiger.jpg") );
-		Icon panther = new ImageIcon( getClass().getResource("panther.jpg") );
-		Icon cheetah = new ImageIcon( getClass().getResource("cheetah.jpg") );
-		Icon cat = new ImageIcon( getClass().getResource("cat.jpg") );
+		Icon lion = new ImageIcon(getClass().getResource("lion.jpg"));
+		Icon tiger = new ImageIcon(getClass().getResource("tiger.jpg"));
+		Icon panther = new ImageIcon(getClass().getResource("panther.jpg"));
+		Icon cheetah = new ImageIcon(getClass().getResource("cheetah.jpg"));
+		Icon cat = new ImageIcon(getClass().getResource("cat.jpg"));
 		
-		label1 = new JLabel(); 
-		label1.setText( "This is a panther" );
-		label1.setIcon( panther ); 
-		label1.setHorizontalTextPosition( SwingConstants.CENTER );
-		label1.setVerticalTextPosition( SwingConstants.BOTTOM );
-		label1.setToolTipText( "This is label for a Panther" );	
-		add( label1 );
+		label1 = new JLabel();
+		label1.setText("This is a panther");
+		label1.setIcon(panther);
+		label1.setHorizontalTextPosition(SwingConstants.CENTER);
+		label1.setVerticalTextPosition(SwingConstants.BOTTOM);
+		label1.setToolTipText("This is the label for a Panther");
+		add(label1);
 		
-		label2 = new JLabel(); 
-		label2.setText( "This is a tiger" );
-		label2.setIcon( tiger ); 
-		label2.setHorizontalTextPosition( SwingConstants.CENTER );
-		label2.setVerticalTextPosition( SwingConstants.BOTTOM );
-		label2.setToolTipText( "This is label for a Tiger" );	
-		add( label2 );
-				
+		label2 = new JLabel();
+		label2.setText("This is a lion");
+		label2.setIcon(lion);
+		label2.setHorizontalTextPosition(SwingConstants.CENTER);
+		label2.setVerticalTextPosition(SwingConstants.BOTTOM);
+		label2.setToolTipText("This is the label for a Lion");
+		add(label2);
+		
 		label3 = new JLabel();
-		label3.setText( "This is a lion" );
-		label3.setIcon( lion ); 
-		label3.setHorizontalTextPosition( SwingConstants.CENTER );
-		label3.setVerticalTextPosition( SwingConstants.BOTTOM );
-		label3.setToolTipText( "This is label for a Lion" );	
-		add( label3 );
+		label3.setText("This is a tiger");
+		label3.setIcon(tiger);
+		label3.setHorizontalTextPosition(SwingConstants.CENTER);
+		label3.setVerticalTextPosition(SwingConstants.BOTTOM);
+		label3.setToolTipText("This is the label for a tiger");
+		add(label3);
 		
-		label4 = new JLabel(); 
-		label4.setText( "This is a cheetah" );
-		label4.setIcon(cheetah );
-		label4.setHorizontalTextPosition( SwingConstants.CENTER );
-		label4.setVerticalTextPosition( SwingConstants.BOTTOM );
-		label4.setToolTipText( "This is label for a Cheetah" );	
-		add( label4 );
+		label4 = new JLabel();
+		label4.setText("This is a cheetah");
+		label4.setIcon(cheetah);
+		label4.setHorizontalTextPosition(SwingConstants.CENTER);
+		label4.setVerticalTextPosition(SwingConstants.BOTTOM);
+		label4.setToolTipText("This is the label for a cheetah");
+		add(label4);
 		
-		label5 = new JLabel(); 
-		label5.setText( "This is a regular cat" );
-		label5.setIcon( cat ); 
-		label5.setHorizontalTextPosition( SwingConstants.CENTER );
-		label5.setVerticalTextPosition( SwingConstants.BOTTOM );
-		label5.setToolTipText( "This is label for a regular cat" );	
-		add( label5 );
-		
+		label5 = new JLabel();
+		label5.setText("This is a cat");
+		label5.setIcon(cat);
+		label5.setHorizontalTextPosition(SwingConstants.CENTER);
+		label5.setVerticalTextPosition(SwingConstants.BOTTOM);
+		label5.setToolTipText("This is the label for a cat");
+		add(label5);
 		
 	}
-
 }
--- src/CatsTest.java
+++ src/CatsTest.java
@@ -1,22 +1,13 @@
-/* Francis Hinson, Binley Yang, Hiroyuki Asaga
- * Lab 17
- * Cats.java, CatsTest.java, lion.jpg, tiger.jpg, panther.jpg, cheetah.jpg, cat.jpg
- * November 11th 2012
- * TAs: Dan Scarafoni & Shuopeng Deng
- * CSC 171
- */
-
 import javax.swing.JFrame;
 
 public class CatsTest {
-
-	public static void main(String[] args) {
+	
+	public static void main (String[] args) {
 		
-		Cats Frame = new Cats(); // create LabelFrame
-		Frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-		Frame.setSize( 800, 800 ); // set frame size
-		Frame.setVisible( true ); // display frame
-
+		Cats frame = new Cats();//this creates the LabelFrame
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 800); // sets the frame size
+		frame.setVisible(true); //displays the frame
+		
 	}
-
 }
