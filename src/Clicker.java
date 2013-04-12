6b8ca386d4c96a392341786b20b28fae1a6bec21
binleyyang <ybinley@gmail.com>
2013-04-12 01:52:13 -0400

    click at random times

--- src/Clicker.java
+++ src/Clicker.java
@@ -1,43 +1,36 @@
 import java.awt.Robot;
 import java.awt.event.InputEvent;
 import java.util.*;
+import java.lang.Math;
 
    public class Clicker {
    
       private static Robot mainBot;
       private int mouseClicks = 0;
       private long OldTime = System.currentTimeMillis();   
+      private static int MAX = 2100;
+      private static int MIN = 1700;
       
-      private int clickTimer = 5000;
+      private int clickTimer = 1900;
       
       public void clickMouse() {
          mainBot.mousePress(InputEvent.BUTTON1_MASK);
          mainBot.mouseRelease(InputEvent.BUTTON1_MASK);
       }
-      
-      public void startBot() {
-         while(true) {
-      
-            if(System.currentTimeMillis() - OldTime > clickTimer) {   
-               OldTime = System.currentTimeMillis();
-               clickMouse();
-               mouseClicks++;
-               System.out.println("Mouse Clicked #" + mouseClicks);
-            }
-         
-         }
-      }
-      
+
       public Clicker() {
-      
-         //System.out.println("Welcome to the Java AutoClicker V" + Constants.version + " created by " + Constants.dontLeech + ".");
-         
-         Scanner sc = new Scanner(System.in);
-         System.out.print("(Seconds) AutoClicker timer:");
-         clickTimer = Integer.parseInt(sc.nextLine()) * 1000;
-         System.out.println("Clicking every " + clickTimer / 1000 + " seconds.");      
-         
-         startBot();
+
+          System.out.println("Clicking every " + clickTimer+ " milliseconds.");      
+          
+          while(true) {
+              if(System.currentTimeMillis() - OldTime > clickTimer) {   
+              	clickTimer = MIN + (int)(Math.random() * (MAX - MIN) + 1);
+                 OldTime = System.currentTimeMillis();
+                 clickMouse();
+                 mouseClicks++;
+                 System.out.println("Mouse Clicked #" + mouseClicks + " every " + clickTimer + " milliseconds.");
+              }        
+           }
       }
       
       public static void main(String args[]) {
